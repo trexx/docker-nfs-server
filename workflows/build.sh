76d23f576da1b474c0baa7c7010a1d980022393a
trexx <874158+trexx@users.noreply.github.com>
2023-12-15 23:11:01 +0100

    Not needed anymore

--- workflows/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-. "../env"
-
-docker build \
-	--build-arg "TAG=$TAG" \
-	--tag "$DOCKER_REPO:$TAG" \
-    "../"
--- workflows/push.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. "../env"
-
-docker push "$DOCKER_REPO:$TAG"
