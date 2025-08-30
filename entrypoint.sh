36d494ba26dcb30c7a53b4a6f91d4f100ef9c7fa
Turan Asikoglu <874158+trexx@users.noreply.github.com>
2025-08-30 13:07:40 +0200

    Update grace-tme to 10s

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ stop() {
 
 mount -a
 
-/usr/sbin/rpc.nfsd --tcp --udp --port 2049 --nfs-version 4.2 --no-nfs-version 3
+/usr/sbin/rpc.nfsd --tcp --udp --port 2049 --grace-time 10 --nfs-version 4.2 --no-nfs-version 3
 /usr/sbin/rpc.mountd --foreground --port 32767 --nfs-version 4.2 --no-nfs-version 2 --no-nfs-version 3 &
 
 wait
